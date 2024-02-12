ff3946638400bb258a6212e4106ee07d4a591164
hfdzhr <21221753116@smkassalaambandung.sch.id>
2024-02-12 14:23:36 +0700

    First Commit

---
+++ lib/app/modules/counter/controllers/counter_controller.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class CounterController extends GetxController {
+  final count = 0.obs;
+
+  void increment() {
+    count.value++;
+  }
+
+  void decrement() {
+    count.value--;
+  }
+}
---
+++ lib/app/modules/counter/views/counter_view.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/counter_controller.dart';
+
+class CounterView extends StatelessWidget {
+  CounterView({Key? key}) : super(key: key);
+  final CounterController controller = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CounterView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Obx(
+          () => Text(
+            'Count : ${controller.count}',
+            style: TextStyle(fontSize: controller.count.toDouble()),
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              if (controller.count > 0) {
+                controller.decrement();
+              } else {
+                Get.snackbar('Peringatan', 'Nilai Minimum 0',
+                    snackPosition: SnackPosition.TOP,
+                    backgroundColor: Colors.deepOrange,
+                    colorText: Colors.white,
+                    icon: Icon(Icons.warning));
+              }
+            },
+            child: const Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              if (controller.count < 100) {
+                controller.increment();
+              } else {
+                Get.snackbar('Peringatan', 'Nilai Maksimal (100)',
+                    snackPosition: SnackPosition.TOP,
+                    backgroundColor: Colors.deepOrange,
+                    colorText: Colors.white,
+                    icon: Icon(Icons.warning));
+              }
+            },
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
